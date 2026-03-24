74d5b75e0ffc4024bf0e90fb152f54c757da8350
TheSmith0803 <ethan.smith@ametek.com>
2026-03-24 08:47:46 -0400

    merging changes

--- vulkan_project/main.cpp
+++ vulkan_project/main.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include <stdexcept>
 #include <cstdlib>
 
@@ -155,21 +156,45 @@ private:
         std::vector<VkPhysicalDevice> devices(deviceCount);
         vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
+        //ordered map to automatically sort physical device candidates
+        std::multimap<int, VkPhysicalDevice> candidates;
+
         for (const auto& device : devices) {
-            if (isDeviceSuitable(device)) {
-                physicalDevice = device;
-                break;
-            }
+            int score = rateDeviceSuitability(device);
+            candidates.insert(std::make_pair(score, device));
         }
 
-        if (physicalDevice == VK_NULL_HANDLE) {
+        if (candidates.rbegin()->first > 0) {
+            physicalDevice = candidates.rbegin()->second;
+            VkPhysicalDeviceProperties deviceProperties;
+            vkGetPhysicalDeviceProperties(physicalDevice, &deviceProperties);
+            std::cout << "Selected GPU: " << deviceProperties.deviceName << std::endl;
+        }
+        else {
             throw std::runtime_error("failed to find a suitable GPU!");
         }
         
     }
 
-    bool isDeviceSuitable(VkPhysicalDevice device) {
-        return true;
+    bool rateDeviceSuitability(VkPhysicalDevice device) {
+        VkPhysicalDeviceProperties deviceProperties;
+        VkPhysicalDeviceFeatures deviceFeatures;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+        int score = 0;
+
+        if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+            score += 1000;
+        }
+
+        score += deviceProperties.limits.maxImageDimension2D;
+
+        if (!deviceFeatures.geometryShader) {
+            return 0;
+        }
+
+        return score;
     }
 
     void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
